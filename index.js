feb377b9eee38a715856c910d7071f87f09139c5
vijayyasaraveni <vijay.yasaraveni@gmail.com>
2023-01-16 15:13:57 +0800

    added default endpoint and error handling

--- index.js
+++ index.js
@@ -10,12 +10,20 @@ app.use(bodyParser.json());
 
 const taskController = require("./controllers/task");
 
+app.get("/", (req, res) => {
+  res.status(200).json({ status: "success", message: "Welcome to the API" });
+});
+
 app.get("/tasks", taskController.getTasks);
 app.post("/tasks", taskController.addTask);
 app.put("/tasks/:id", taskController.updateTask);
 app.delete("/tasks/:id", taskController.deleteTask);
 
 // app.use(morgan("combined", { stream: accessLogStream }));
+
+app.use((req, res) => {
+  res.status(404).json({ status: "error", message: "404 Not Found" });
+});
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");
